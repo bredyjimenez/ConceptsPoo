7aee1e00c8a07af3970397c3accce0b1676b57ad
Bredy Jimenez <bredy.jimenez03@gmail.com>
2025-01-15 22:01:57 -0400

    I created the methods to validate days and month and isleapyear and control exceptions

---
+++ ConceptoPoo/ConceptoPoo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptoPoo;
+
+Console.WriteLine("Concept Poo");
+Console.WriteLine("===========");
+try
+{
+	Console.WriteLine(new Date(2025, 2, 22));
+	Console.WriteLine(new Date(2021, 7, 25));
+	Console.WriteLine(new Date(1989, 11, 30));
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);
+}
